d04ba27c88399f625ac617e027a6b8e97b43d3c7
DineshPythonista <sds2134591@gmail.com>
2023-10-15 13:07:28 +0530

    booking func

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const User = require("./models/User.js");
 const Place = require("./models/Place.js");
+const Booking = require("./models/Booking.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
@@ -196,6 +197,26 @@ app.put("/places", async (req, res) => {
 
 app.get("/places", async (req, res) => {
   res.json(await Place.find());
+});
+
+app.post("/bookings", (req, res) => {
+  const { place, checkIn, checkOut, numberOfGuests, name, phone, price } =
+    req.body;
+  Booking.create({
+    place,
+    checkIn,
+    checkOut,
+    numberOfGuests,
+    name,
+    phone,
+    price,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      throw err;
+    });
 });
 
 app.listen(4000);
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function IndexPage() {
   const [places, setPlaces] = useState([]);
   useEffect(() => {
